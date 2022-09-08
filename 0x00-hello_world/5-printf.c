eeffd1fc198875214de542949b9e4bbf99bd56fb
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-09-08 14:55:33 -0700

    assembler

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * int main: calls on the function
+ *
+ * printf: prints the function
+ *
+ * return: if succes, return 0
+ */
+int main (void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return 0;
