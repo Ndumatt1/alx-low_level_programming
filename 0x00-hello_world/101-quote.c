72eb09cb2fc8843c3b6a8b19d3eb6441edf7b7f3
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-09-09 14:32:25 -0700

    corrected_quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,8 +3,9 @@
 
 /**
  * main - prints exactly "and that piece of art is useful"
- *
- * Rerurn: Always 0 (success)
+ * -Dora Korpar, 2015 - 10 - 19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (success)
  */
 
 int main(void)
