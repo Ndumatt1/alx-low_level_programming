2a9f342db33ce8aab87ec17d1fa3756d0dda7ff9
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-09-13 13:02:07 -0700

    loops_nesting

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints _putchar
+ * Return: Always 0 (Success)
+ */
+
+char main(void)
+{
+
+	putchar("_putchar\n");
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+void print_alphabet(void)
+/**
+ * main - prints alphabet in lowercase followed by new line
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+
+		putchar("ch");
+	putchar('\n');
+	print_alphabet();
+	return (0);
+}
