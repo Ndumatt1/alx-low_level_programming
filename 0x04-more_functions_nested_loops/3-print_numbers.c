a2ed961077bbb44b625985649401eaa508c648ee
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-09-15 15:53:29 -0700

    work on print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,10 +8,9 @@ void print_numbers(void)
 {
 	int i;
 
-	if (i >= 0 && i <= 9)
-		i++;
+	for (i = '0'; i <= '9'; i++)
 		{
 			_putchar(i);
 		}
-	_putchar("\n");
+	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,11 @@
+#ifndef MAIN_FILE
+#define MAIN_FILE
+
+int _putchar(char c);
 int _isupper(int);
 int _isdigit(int);
 int mul(int a, int b);
 void print_numbers(void);
+
+
+#endif
