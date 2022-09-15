faabaa5c372c18b3a6b8caa6bcf7cfc9fcb032fe
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-09-15 10:28:33 -0700

    debugging ammend

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -15,11 +15,11 @@ int largest_number(int a, int b, int c)
 	{
 	largest = a;
 	}
-	else if (b > a && a >= c)
+	else if (b >= a && a >= c)
 	{
 	largest = b;
 	}
-	else (c >= a && a >= b)
+	else
 	{
 	largest = c;
 	}
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,4 +4,5 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
