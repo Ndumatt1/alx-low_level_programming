4659998ef3d9b578cc1d0ecb27759af8f6ba413e
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-09-15 04:17:32 -0700

    ammend debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,14 +7,9 @@
  */
 int main(void)
 {
-	int n;
+	int i;
 
-	srand(time(0));
-
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0);
-	printf("%d is zero\n", n);
+	i = 98;
+	positive_or_negative(i);
 	return (0);
 }
