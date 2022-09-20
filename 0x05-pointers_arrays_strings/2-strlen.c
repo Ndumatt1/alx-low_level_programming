413c778c6dfbf0a9a43f82549c94c67a2cf8a538
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-09-20 04:41:16 -0700

    stringlenghth

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - returns lenght of string
+ *
+ * @s: pointer to the string
+ */
+
+int _strlen(char *s)
+{
+	s = strlen;
+
+	_putchar("strlen %d", s);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,5 +4,7 @@
 int _putchar();
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
+#include <string.h>
 
 #endif
