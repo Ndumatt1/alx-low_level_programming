4d347f58fb234b7427d30c10a577b4db51553c76
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-10-11 07:12:36 -0700

    defined a new type dog_t for type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - struct type
+ *
+ * @name: first member
+ *
+ * @age: second member
+ *
+ * @owner: third member
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+typedef struct dog dog_t;
 
 #endif
