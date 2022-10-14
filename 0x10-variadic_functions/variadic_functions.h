91971d4afb2a640ff1a24866be98a31ad7fe2481
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-10-14 03:36:49 -0700

    headerfile update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,9 @@
 #ifndef VARIADIC_FUCNTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-int _putchar(char c);
+int _putchar();
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 
 #endif
