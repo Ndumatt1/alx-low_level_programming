6ea1a37f6cc70b34c3572c6d6b011f0968949868
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-10-14 04:15:40 -0700

    fixed erro on 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,11 @@
 #include "function_pointers.h"
 
 /**
+ * print_name - prints name
  *
+ * @name: name to be printed
+ *
+ * @f: functions pointer
  */
 
 void print_name(char *name, void (*f)(char *))
