2a704d7a58d7454d1baef5dd43d436f96840120e
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-10-15 11:47:42 -0700

    fixed error on 2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,32 +13,29 @@
 
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	char *str;
 	unsigned int i;
-	unsigned int len;
-	unsigned int j;
-
+	char *str;
 	va_list ptr;
 
 	va_start(ptr, n);
+
 	for (i = 0; i < n; i++)
 	{
-		str = va_arg(ptr, char*);
-		printf("%s\n", str);
-	}
-	va_end(ptr);
+		str = va_arg(ptr, char *);
 
-	for (j = 0; str[j] != '\0'; len++)
-	{
-		str[j] = len;
-	}
-	if (i < n + 1 && separator != NULL)
-	{
-		printf("%s\n", separator);
-	}
-	if (str[j] == '\0')
-	{
-		printf("(nil)");
+		if (!str)
+			str = "(nil)";
+
+	if (!separator)
+
+		printf("%s", str);
+	else if (separator && i == 0)
+
+		printf("%s", str);
+	else
+		printf("%s%s", separator, str);
+
 	}
 	printf("\n");
+	va_end(ptr);
 }
