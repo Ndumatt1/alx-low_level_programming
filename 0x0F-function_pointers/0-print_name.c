b54cd47e5bb3e4af9da85f9a00a382b2474c15ba
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-10-14 18:08:12 -0700

    hot fix on 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -10,5 +10,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	 (*f)(name);
+	if (name == NULL && f == NULL)
+		return;
+	f(name);
 }
--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,9 +15,12 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned long int i;
 
-	for (i = 0; i < size; i++)
+	if (action && array)
 	{
-		(action)(array[i]);
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
 	}
 
 }
