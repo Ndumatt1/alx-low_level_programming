90b26a354c692ed8fd6451cbb75a5c4a8aa173c0
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-10-22 14:38:53 -0700

    updated  2-add_node.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -15,7 +15,9 @@ size_t print_list(const list_t *h)
 	while (h != NULL)
 	{
 		if (h->str == NULL)
+		{
 			printf("[%d] %s\n", 0, "(nil)");
+		}
 		else
 		{
 			printf("[%d] %s", h->len, h->str);
---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a new node to linked list
+ *
+ * @head: head of the linked list
+ *
+ * @str: string of the linked list
+ *
+ * Return: the addres of the new element or NULL if it fails
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *temp;
+	size_t n;
+
+	temp = malloc(sizeof(list_t));
+
+	if (!temp)
+		return (NULL);
+
+	temp->str = strdup(str);
+
+	for (n = 0; str[n]; n++)
+		temp->len = n;
+		temp->next = *head;
+		*head = temp;
+	return (*head);
+}
