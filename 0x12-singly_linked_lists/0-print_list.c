fc4b4d820b0e211f831a0071fb1b41af6ad5bef0
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-10-22 09:09:29 -0700

    updates singly linked lists 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of list_t
+ *
+ * @h: pointer of list_t
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int len = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+		{
+			printf("[%d] %s", h->len, h->str);
+			h = h->next;
+			len++;
+		}
+	}
+	return (len);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This directory contains projects on singly linked list in C programming language.
