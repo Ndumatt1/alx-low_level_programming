0f9f163bc70df4ae91559f5477d00443978b4682
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-11-01 22:47:22 +0000

    fixed error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,23 +8,27 @@
  * @text_content: NuLL terminated string to write to the file
  *
  * Return: 1 on success -1 on failure
- *
  */
 
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	int num = 0;
+	ssize_t count = 0;
 
 	if (!filename)
 		return (-1);
+
 	if (!text_content)
 	fd = open(filename, O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
+	num = read(fd, text_content, count);
+		if (!num)
+			return (0);
 
-	num = write(STDOUT_FILENO, text_content, num);	
+	num = write(STDOUT_FILENO, text_content, num);
 	if (!num)
 		return (-1);
 
