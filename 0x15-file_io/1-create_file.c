1dc3806b6e173c195d045586536ec0c3bd2cccbd
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-11-01 22:25:17 +0000

    hot fix on 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,18 +14,20 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int size = 0;
+	int num = 0;
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 600);
+	if (!filename)
+		return (-1);
+	if (!text_content)
+	fd = open(filename, O_CREAT | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	write(fd, "text_content", size);
+	num = write(STDOUT_FILENO, text_content, num);	
+	if (!num)
+		return (-1);
 
-	if (text_content == NULL)
-
-		close(fd);
-
+	close(fd);
 	return (1);
 }
