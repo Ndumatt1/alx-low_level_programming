0bc72d678043598366eec7a6e46b5cf730b59d22
Ndumatt1 <ndubuisimathew8@gmail.com>
2022-12-13 16:48:19 +0100

    fixed bug on 0-print_dlistint

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -2,6 +2,11 @@
 #include "lists.h"
 #include <stdio.h>
 
+/**
+* print_dlistint - prints a double linked list
+* @h: head pointer to doubly linked list
+* Return: number of nodes
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	const dlistint_t *current;
