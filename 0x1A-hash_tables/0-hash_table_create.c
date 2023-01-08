b87ea3534bfb1c3f1040220ce935c40502203799
Ndumatt1 <ndubuisimathew8@gmail.com>
2023-01-08 05:07:16 -0800

    fux bug on 0-hash_table_create

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,20 +6,21 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table;
-	unsigned long int i;
+	hash_table_t *hash_table = NULL;
 
-	hash_table = malloc(sizeof(hash_table_t));
-	if (!hash_table)
-		return (NULL);
-	hash_table->array = malloc(sizeof(hash_node_t) * size);
-	if (!hash_table->array)
+	if (size != 0)
 	{
-		free(hash_table);
-		return (NULL);
+		hash_table = malloc(sizeof(hash_table_t));
+		if (hash_table == NULL)
+		{
+			return (NULL);
+		}
+		hash_table->size = size;
+		hash_table->array = calloc(hash_table->size, sizeof(hash_node_t *));
+		if (hash_table->array == NULL)
+		{
+			return (NULL);
+		}
 	}
-	for (i = 0; i < size; i++)
-		hash_table->array[i] = NULL;
-	hash_table->size = size;
 	return (hash_table);
 }
